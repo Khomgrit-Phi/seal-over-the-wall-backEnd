ee7df18849ca4dad067b323f9d052621e9a3880f
JLezzzzz <teeramaet.w@gmail.com>
2025-05-08 01:53:53 +0700

    feat(file): Re-define Order&Cart schema and how the creation flow work. Added routes for order and cart.

--- models/Order.js
+++ models/Order.js
@@ -2,12 +2,14 @@ import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
 const OrderItemSchema = new Schema({
+    orderId: { type: Schema.Types.ObjectId, ref: 'Order', required: true },
     productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
     quantity: { type: Number, required: true },
     unitPrice: { type: Number, required: true },
     totalPrice: { type: Number, default: function () { return this.quantity * this.unitPrice } },
     selectedSize: { type: String, required: true },
-    selectedColor: { type: String, required: true }
+    selectedColor: { type: String, required: true },
+    addedAt: { type: Date, default: Date.now }
   }, { _id: true });
 
   OrderItemSchema.pre("save", function (next) {
@@ -15,18 +17,17 @@ const OrderItemSchema = new Schema({
     next();
 });
 
-
 const OrderSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    addressId: { type: Schema.Types.ObjectId, required: true },
-    items: { type: [OrderItemSchema], required: true },
+    items: { type: [OrderItemSchema], required: true, default: [] },
+    addressId: { type: Schema.Types.ObjectId, ref:"Address" },
     status: {
       type: String,
       enum: ['pending', 'paid', 'shipped', 'delivered', 'cancelled'],
       default: 'pending'
     },
-    total: { type: Number, required: true },
-    vat: { type: Number, required: true },
+    total: { type: Number, default: 0 },
+    vat: { type: Number, default:7 },
     paymentId: { type: Schema.Types.ObjectId, ref: 'Payment' },
     orderDate: { type: Date, default: Date.now }
   });
