7e0e63f11f5282c478d7f6a730ac7a805eba33e8
May616388 <mayochapong.a@gmail.com>
2025-05-09 11:33:14 +0700

    feat(file): Updated cartRoute.js cart.rest Product.js model

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,9 @@
 import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
-<<<<<<< HEAD
 const ColorSchema = new Schema(
   {
     productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-=======
-const ColorSchema = new Schema({
-    productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
->>>>>>> origin/develop
     colorName: { type: String, required: true },
     colorCode: { type: String, required: true },
     image: { type: [String], required: true },
@@ -17,13 +12,14 @@ const ColorSchema = new Schema({
 );
 
 const ProductSchema = new Schema({
-<<<<<<< HEAD
   productType: { type: String, required: true },
   styleName: { type: String, required: true },
   title: {
     type: String,
     required: true,
-    default: { styleName } + " - " + { productType },
+    default: function () {
+      return this.styleName + " - " + this.productType;
+    },
   },
   description: { type: String },
   price: { type: Number, default: 499, required: true },
@@ -31,17 +27,6 @@ const ProductSchema = new Schema({
   color: { type: [ColorSchema], required: true },
   tag: { type: [String], default: [] },
   createdOn: { type: Date, default: new Date().getTime() },
-=======
-    productType: { type: String, required: true },
-    styleName: { type: String, required: true },
-    title: { type: String, required:true, default: function () { return this.styleName +" - " + this.productType } },
-    description: { type: String },
-    price: { type: Number, default: 499, required:true },
-    size: { type: [String], required: true },
-    color: { type: [ColorSchema], required: true },
-    tag: { type: [String], default:[] },
-    createdOn: { type: Date, default: new Date().getTime() },
->>>>>>> origin/develop
 });
 
 export const Product = model("Product", ProductSchema);
