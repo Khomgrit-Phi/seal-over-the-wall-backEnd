91a9ce5d40a7e94331d7e7886223247e4e5fc83e
JLezzzzz <teeramaet.w@gmail.com>
2025-05-11 23:17:47 +0700

    Merge with K'May

--- models/Order.js
+++ models/Order.js
@@ -15,7 +15,7 @@ const OrderItemSchema = new Schema(
     },
     selectedSize: { type: String, required: true },
     selectedColor: { type: String, required: true },
-    selectedImage: { type: [String], default: [] },
+    selectedImage: { type: String, required: true },
     addedAt: { type: Date, default: Date.now },
   },
   { _id: true }
